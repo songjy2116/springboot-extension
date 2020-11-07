500afdb3e32c189a8918d31d29444ddd75a6fe00
songjy2116 <songjy2116@163.com>
2020-11-07 23:51:03 +0800

    callrunners 扩展

--- src/main/java/com/ymkz/WuchenlinApplication.java
+++ src/main/java/com/ymkz/WuchenlinApplication.java
@@ -1,13 +1,30 @@
 package com.ymkz;
 
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class WuchenlinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WuchenlinApplication.class, args);
+		List<String> paramsList = new ArrayList<>();
+		paramsList.add("MyApplicationRunner");
+		paramsList.add("MyCommandLineRunner");
+		paramsList.add("--server.port=8081");
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(WuchenlinApplication.class, paramsList.toArray(new String[0]));
+
+		DefaultApplicationArguments applicationArguments = (DefaultApplicationArguments)applicationContext.getBean("springApplicationArguments");
+		Set<String> optionNames = applicationArguments.getOptionNames();
+		List<String> nonOptionArgs = applicationArguments.getNonOptionArgs();
+		optionNames.stream().forEach(System.out::println);
+		System.out.println("-----------------------------------");
+		nonOptionArgs.stream().forEach(System.out::println);
 	}
 
 }
---
+++ src/main/java/com/ymkz/callRunners/MyApplicationRunner.java
@@ -0,0 +1,15 @@
+package com.ymkz.callRunners;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(Arrays.toString(args.getSourceArgs()));
+    }
+}
